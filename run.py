83047c48a62ca2194a3343f8846db74313dc315e
fumitrial8 <fumitrial8@gmail.com>
2020-04-18 20:47:21 +0900

    make UI

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def home():
+    return "<form action='/analize' method='post'><label>分析したい単語を入力<input type='text' name='tweet'></label><input type='submit' value='送信'></form>"
+
+@app.route('/analize', methods=["POST"])
+def analize():
+    data = request.form["tweet"]
+    return "<h1>分析結果</h1><h1>%s</h1><a href='/'>戻る</a>" % data
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, host='0.0.0.0', port=5000)
